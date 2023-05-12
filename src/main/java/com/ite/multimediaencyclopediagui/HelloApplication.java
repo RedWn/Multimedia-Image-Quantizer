05d5e22ee362d79de75fa053de2181b0ebe26842
AntonDirani <antondirani@outlook.com>
2023-05-12 14:14:20 +0300

    Editing the layout, adding the imagesViewers

--- src/main/java/com/ite/multimediaencyclopediagui/HelloApplication.java
+++ src/main/java/com/ite/multimediaencyclopediagui/HelloApplication.java
@@ -12,6 +12,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -21,6 +23,7 @@ import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+
 
 public class HelloApplication extends Application {
     Stage window;
@@ -32,10 +35,29 @@ public class HelloApplication extends Application {
 
         FileChooser fileChooser = new FileChooser();
         Button uploadImageButton = new Button("Choose a photo");
+        ImageView imageView1 = new ImageView();
+        ImageView imageView2 = new ImageView();
+
+
+        Image defaultImage = new Image("default_image.png");
+        imageView1.setImage(defaultImage);
+        imageView2.setImage(defaultImage);
+        imageView1.setFitWidth(250);
+        imageView1.setFitHeight(250);
+        imageView2.setFitWidth(250);
+        imageView2.setFitHeight(250);
 
         uploadImageButton.setOnAction(e -> {
             try {
                 File chosenFile = fileChooser.showOpenDialog(stage);
+
+
+                Image image = new Image(chosenFile.toURI().toString());
+                imageView1.setImage(image);
+                imageView1.setPreserveRatio(true);
+                imageView1.setFitWidth(250);
+                imageView1.setFitHeight(250);
+
                 BufferedImage myPicture = ImageIO.read(chosenFile);
                 Pixel[] newImage = MedianCut.Algorithm(ImageManipulation.ImageToMatrix(myPicture), 16);
 
@@ -53,10 +75,19 @@ public class HelloApplication extends Application {
         separator.setValignment(VPos.CENTER);
         separator.setPadding(new Insets(10));
 
-        Label label1 = new Label("Here goes image1");
-        Label label2 = new Label("Here goes image2");
+        Label label1 = new Label("Before");
+        Label label2 = new Label("After");
+        VBox imageVBox1 = new VBox();
+        VBox imageVBox2 = new VBox();
+        imageVBox1.setAlignment(Pos.CENTER);
+        imageVBox2.setAlignment(Pos.CENTER);
+        imageVBox1.getChildren().addAll(label1, imageView1);
+        imageVBox2.getChildren().addAll(label2, imageView2);
+
+
+
         hBox.setAlignment(Pos.CENTER);
-        hBox.getChildren().addAll(label1, separator, label2);
+        hBox.getChildren().addAll(imageVBox1, separator, imageVBox2);
 
         VBox vBox = new VBox();
         vBox.setAlignment(Pos.CENTER);
@@ -68,7 +99,7 @@ public class HelloApplication extends Application {
         StackPane layout = new StackPane();
         layout.getChildren().addAll(vBox);
 
-        stage.setScene(new Scene(layout, 500, 500));
+        stage.setScene(new Scene(layout, 800, 500));
         stage.show();
     }
 

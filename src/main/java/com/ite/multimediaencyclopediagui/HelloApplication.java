75f85d91441dc77c10024a36856e0effb5a34137
Hasan Mothaffar <hasan.mozafar@gmail.com>
2023-05-12 07:41:02 +0300

    Upload images from local directory

--- src/main/java/com/ite/multimediaencyclopediagui/HelloApplication.java
+++ src/main/java/com/ite/multimediaencyclopediagui/HelloApplication.java
@@ -4,26 +4,39 @@ import com.ite.multimediaencyclopediagui.images.ImageManipulation;
 import com.ite.multimediaencyclopediagui.images.MedianCut;
 import com.ite.multimediaencyclopediagui.images.Pixel;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        BufferedImage myPicture = ImageIO.read(new File("test.jpg"));
-        Pixel[] newImage = MedianCut.Algorithm(ImageManipulation.ImageToMatrix(myPicture), 16);
-        ImageIO.write(ImageManipulation.MatrixToImage(newImage, myPicture), "jpg", new File("new-test.jpg"));
+    public void start(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        Button uploadImageButton = new Button("Upload Image");
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1500, 700);
-        stage.setTitle("Multimedia Project");
-        stage.setScene(scene);
+        uploadImageButton.setOnAction(e -> {
+            try {
+                File chosenFile = fileChooser.showOpenDialog(stage);
+                BufferedImage myPicture = ImageIO.read(chosenFile);
+                Pixel[] newImage = MedianCut.Algorithm(ImageManipulation.ImageToMatrix(myPicture), 16);
+
+                ImageIO.write(ImageManipulation.MatrixToImage(newImage, myPicture), "jpg", new File("new-test.jpg"));
+
+            } catch (Exception exception) {
+                System.out.println(exception);
+            }
+        });
+
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(uploadImageButton);
+
+        stage.setScene(new Scene(layout, 500, 500));
         stage.show();
     }
 

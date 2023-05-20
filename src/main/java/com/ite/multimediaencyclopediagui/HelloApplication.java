206838ed2150a9d18f69705ef82277e3db5c11b3
IyadAlanssary <77411974+IyadAlanssary@users.noreply.github.com>
2023-05-20 22:20:44 +0300

    Update viewing method to a pop up

--- src/main/java/com/ite/multimediaencyclopediagui/HelloApplication.java
+++ src/main/java/com/ite/multimediaencyclopediagui/HelloApplication.java
@@ -2,11 +2,8 @@ package com.ite.multimediaencyclopediagui;
 
 import com.ite.multimediaencyclopediagui.images.*;
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -32,7 +29,7 @@ public class HelloApplication extends Application {
     /**
      * Directory where images are stored after applying the algorithm.
      */
-    private final SimpleStringProperty resultsDirectory = new SimpleStringProperty();
+    private String resultsDirectory = new String();
 
     public static void main(String[] args) {
         launch();
@@ -53,13 +50,8 @@ public class HelloApplication extends Application {
         imageViewFirstAlgo.setImage(placeholderImage);
         imageViewSecondAlgo.setImage(placeholderImage);
 
-        imageViewOriginal.setFitWidth(250);
-        imageViewFirstAlgo.setFitWidth(250);
-        imageViewSecondAlgo.setFitWidth(250);
-
-        imageViewOriginal.setFitHeight(250);
-        imageViewFirstAlgo.setFitHeight(250);
-        imageViewSecondAlgo.setFitHeight(250);
+        //hard-coded, for speedy reasons
+        resultsDirectory = "D:\\";
 
         imageViewOriginal.setPreserveRatio(true);
         imageViewFirstAlgo.setPreserveRatio(true);
@@ -67,11 +59,12 @@ public class HelloApplication extends Application {
 
         DirectoryChooser directoryChooser = new DirectoryChooser();
         Text resultsDirectoryTextNode = new Text();
-        Button chooseDirectoryButton = new Button("Choose a directory");
+        resultsDirectoryTextNode.setText("Selected directory: " + resultsDirectory);
 
+        Button chooseDirectoryButton = new Button("Choose a directory");
         chooseDirectoryButton.setOnAction(e -> {
             File chosenDirectory = directoryChooser.showDialog(stage);
-            this.resultsDirectory.set(chosenDirectory.toString());
+            this.resultsDirectory = chosenDirectory.toString();
             resultsDirectoryTextNode.setText("Selected directory: " + chosenDirectory);
         });
 
@@ -81,7 +74,7 @@ public class HelloApplication extends Application {
         uploadImageButton.setOnAction(e -> {
             try {
                 File chosenFile = fileChooser.showOpenDialog(stage);
-                System.out.println(resultsDirectory.getValue());
+                System.out.println(resultsDirectory);
 
                 Image image = new Image(chosenFile.toURI().toString());
                 imageViewOriginal.setImage(image);
@@ -97,8 +90,8 @@ public class HelloApplication extends Application {
                 BufferedImage bufferedQuantizedImage2 = ImageUtils.PixelsToImage(quantizedPixels2, originalPicture.getWidth(), originalPicture.getHeight(), originalPicture.getType());
                 Image nonBufferedQuantizedImageToMakeJavaHappy2 = ImageUtils.ConvertBufferedImageToImage(bufferedQuantizedImage2);
 
-                String pathname = Path.of(resultsDirectory.getValue(), "new-test.jpg").toString();
-                String pathname2 = Path.of(resultsDirectory.getValue(), "new-test2.jpg").toString();
+                String pathname = Path.of(resultsDirectory, "new-test.jpg").toString();
+                String pathname2 = Path.of(resultsDirectory, "new-test2.jpg").toString();
                 IOIndexed.writeIndexed(bufferedQuantizedImage, pathname);
                 IOIndexed.writeIndexed(bufferedQuantizedImage2, pathname2);
 
@@ -112,39 +105,37 @@ public class HelloApplication extends Application {
                 imageViewFirstAlgo.setImage(nonBufferedQuantizedImageToMakeJavaHappy);
                 imageViewSecondAlgo.setImage(nonBufferedQuantizedImageToMakeJavaHappy2);
 
+                Stage popUpStage = new Stage();
+                popUpStage.initOwner(window);
+                HBox hBox = new HBox(10);
+
+                imageViewOriginal.setFitWidth(500);
+                imageViewFirstAlgo.setFitWidth(500);
+                imageViewSecondAlgo.setFitWidth(500);
+
+                hBox.getChildren().addAll(imageViewOriginal, imageViewFirstAlgo, imageViewSecondAlgo);
+                hBox.setAlignment(Pos.CENTER);
+
+                Label label1 = new Label("Original");
+                Label label2 = new Label("1st Algorithm");
+                Label label3 = new Label("2nd Algorithm");
+                HBox hBoxLabels = new HBox(350);
+                hBoxLabels.setAlignment(Pos.CENTER);
+                hBoxLabels.getChildren().addAll(label1, label2, label3);
+
+                VBox vBox = new VBox(10);
+                vBox.getChildren().addAll(hBoxLabels, hBox);
+
+                Scene popUpScene = new Scene(vBox,  1500, 400);
+
+                popUpStage.setTitle("Algorithms");
+                popUpStage.setScene(popUpScene);
+                popUpStage.show();
+
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
-
-
-        Separator separator1 = new Separator();
-        separator1.setOrientation(Orientation.VERTICAL);
-        separator1.setPrefHeight(80);
-        separator1.setValignment(VPos.CENTER);
-        separator1.setPadding(new Insets(10));
-
-        Separator separator2 = new Separator();
-        separator2.setOrientation(Orientation.VERTICAL);
-        separator2.setPrefHeight(80);
-        separator2.setValignment(VPos.CENTER);
-        separator2.setPadding(new Insets(10));
-
-        Label label1 = new Label("Original");
-        Label label2 = new Label("1st Algorithm");
-        Label label3 = new Label("2nd Algorithm");
-
-        VBox imageVBox1 = new VBox();
-        VBox imageVBox2 = new VBox();
-        VBox imageVBox3 = new VBox();
-
-        imageVBox1.setAlignment(Pos.CENTER);
-        imageVBox2.setAlignment(Pos.CENTER);
-        imageVBox3.setAlignment(Pos.CENTER);
-
-        imageVBox1.getChildren().addAll(label1, imageViewOriginal);
-        imageVBox2.getChildren().addAll(label2, imageViewFirstAlgo);
-        imageVBox3.getChildren().addAll(label3, imageViewSecondAlgo);
 
         Label label = new Label("Choose an Image:");
         // Create the radio buttons
@@ -166,10 +157,6 @@ public class HelloApplication extends Application {
         vBoxRadioButtons.setPadding(new Insets(10));
         vBoxRadioButtons.getChildren().addAll(originalImageRadioButton, FirstAlgoRadioButton, SecondAlgoRadioButton);
 
-        HBox imagesContainer = new HBox();
-        imagesContainer.setAlignment(Pos.CENTER);
-        imagesContainer.getChildren().addAll(imageVBox1, separator1, imageVBox2, separator2, imageVBox3);
-
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.CENTER);
         hBox.setSpacing(50);
@@ -178,13 +165,13 @@ public class HelloApplication extends Application {
         VBox appContainer = new VBox();
         appContainer.setAlignment(Pos.CENTER);
         appContainer.setPadding(new Insets(10));
-        appContainer.setSpacing(10);
-        appContainer.getChildren().addAll(chooseDirectoryButton, resultsDirectoryTextNode, uploadImageButton, imagesContainer, hBox);
+        appContainer.setSpacing(15);
+        appContainer.getChildren().addAll(chooseDirectoryButton, resultsDirectoryTextNode, uploadImageButton, hBox);
 
         StackPane layout = new StackPane();
         layout.getChildren().addAll(appContainer);
 
-        stage.setScene(new Scene(layout, 800, 500));
+        stage.setScene(new Scene(layout, 600, 300));
         stage.show();
     }
 }

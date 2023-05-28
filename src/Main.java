e00712722a931ac6bd8f8a2051fa9445cb1f28ff
RedWn <redwaniscool@gmail.com>
2023-05-28 12:37:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,41 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Dictionary;
-import java.util.Vector;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int nColors = 32;
-        BufferedImage myPicture = ImageIO.read(new File("test2.jpg"));
-        Pixel[] newImage = LloydsAlgorithm.GetQuantizedPixels(ImageManipulation.ImageToMatrix(myPicture), 4);
-        ImageIO.write(ImageManipulation.MatrixToImage(newImage, myPicture), "jpg", new File("new-test.jpg"));
+        int nColors = 64;
 
-        writeIndexed(newImage, myPicture.getWidth(), myPicture.getHeight(), nColors, new File("output.rii"));
+//        File ff = new File("./tests");
+//            int i=0;
+//        for (File f: Objects.requireNonNull(ff.listFiles())) {
+//            BufferedImage myPicture = ImageIO.read(f);
+//            Pixel[] newImage = MedianCut.Algorithm(ImageManipulation.ImageToMatrix(myPicture), nColors);
+//            IOIndexed.writeIndexed(ImageManipulation.MatrixToImage(newImage, myPicture.getWidth(),myPicture.getHeight(),myPicture.getType()), nColors, String.format("output(%d).rii", i++));
+//        }
+//        for (int i=0;i<22;i++){
+//            IndexedImage newImage2 = IOIndexed.readIndexed(String.format("./riitests/output(%d).rii", i));
+//            ImageIO.write(ImageManipulation.MatrixToImage(newImage2.pixels, newImage2.width,newImage2.height,5), "jpg", new File(String.format("output/new-test(%d).jpg", i)));
+//        }
 
-    }
+//        Pixel[] newImage = LloydsAlgorithm.GetQuantizedPixels(ImageManipulation.ImageToMatrix(myPicture), nColors);
+//        IndexedImage newImage = IOIndexed.readIndexed("output.rii");
+//        ImageIO.write(ImageManipulation.MatrixToImage(newImage, myPicture.getWidth(), myPicture.getHeight(), myPicture.getType()), "jpg", new File("new-test2.jpg"));
 
-    public static void writeIndexed(Pixel[] image,int width,int height,int nColors,File file) throws IOException {
-        String fileName = "output.rii";
-        writeBinaryStringToFile(padder(888,16),fileName);
-        writeBinaryStringToFile(padder(width,16),fileName);
-        writeBinaryStringToFile(padder(height,16),fileName);
+//        IndexedImage newImage2 = IOIndexed.readIndexed("output2.rii");
+//        ImageIO.write(ImageManipulation.MatrixToImage(newImage2.pixels, newImage2.width,newImage2.height,myPicture.getType()), "jpg", new File("new-test2.jpg"));
 
-        int[] colorsRepetition = new int[nColors];
-        Vector<Integer> colors = new Vector<Integer>();
-        for (int i=0;i<image.length;i++){
-            int color = ImageManipulation.convertFromRGBArray(image[i].RGB);
-            if (colors.contains(color)){
-                colorsRepetition[colors.indexOf(color)]++;
-                writeBinaryStringToFile(padder(colors.indexOf(color),8),fileName);
-            }
-            else {
-                colors.add(color);
-                writeBinaryStringToFile(padder(colors.indexOf(color),8),fileName);
-            }
+
+
+        Searcher.setTarget("drive2.rii");
+        File[] fff = Searcher.Search("./riitests");
+        int i=0;
+        for (File ff: fff){
+            IndexedImage newImage2 = IOIndexed.readIndexed(ff.getAbsolutePath());
+            ImageIO.write(ImageManipulation.MatrixToImage(newImage2.pixels, newImage2.width,newImage2.height,5), "jpg", new File(String.format("output/888new-test(%d).jpg", i++)));
         }
-        for (Integer color : colors){
-            writeBinaryStringToFile(padder(colors.indexOf(color),8),fileName);
-            writeBinaryStringToFile(padder(color,8),fileName);
-            writeBinaryStringToFile(padder(colorsRepetition[colors.indexOf(color)],8),fileName);
-        }
-    }
 
-    public static String padder(int number, int length) {
-        String string = Integer.toBinaryString(number);
-        return "0".repeat(Math.max(0, length - string.length())) + string;
-    }
-
-    public static void writeBinaryStringToFile(String binaryString, String fileName) throws IOException {
-        int value = Integer.parseInt(binaryString, 2);
-        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(fileName))) {
-            dos.writeInt(value);
-        }
     }
 }

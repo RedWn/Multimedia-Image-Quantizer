46fcc3b274fd1ca0dda2d197a5708278e7b52bb3
RedWn <redwaniscool@gmail.com>
2023-05-29 22:05:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,17 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int nColors = 64;
+        int nColors = 16;
 
 //        File ff = new File("./tests");
 //            int i=0;
 //        for (File f: Objects.requireNonNull(ff.listFiles())) {
 //            BufferedImage myPicture = ImageIO.read(f);
 //            Pixel[] newImage = MedianCut.Algorithm(ImageManipulation.ImageToMatrix(myPicture), nColors);
-//            IOIndexed.writeIndexed(ImageManipulation.MatrixToImage(newImage, myPicture.getWidth(),myPicture.getHeight(),myPicture.getType()), nColors, String.format("output(%d).rii", i++));
+//            IOIndexed.writeIndexed(ImageManipulation.MatrixToImage(newImage, myPicture.getWidth(),myPicture.getHeight(),myPicture.getType()), String.format("./riitests/output(%d).rii", i++));
 //        }
+
+
 //        for (int i=0;i<22;i++){
 //            IndexedImage newImage2 = IOIndexed.readIndexed(String.format("./riitests/output(%d).rii", i));
 //            ImageIO.write(ImageManipulation.MatrixToImage(newImage2.pixels, newImage2.width,newImage2.height,5), "jpg", new File(String.format("output/new-test(%d).jpg", i)));
@@ -29,12 +31,14 @@ public class Main {
 
 
 
-        Searcher.setTarget("drive2.rii");
+        Searcher.setTarget("./riitests/output(16).rii",3);
+        Searcher.color = 5f;
+        Searcher.per = 0.005f;
         File[] fff = Searcher.Search("./riitests");
         int i=0;
         for (File ff: fff){
             IndexedImage newImage2 = IOIndexed.readIndexed(ff.getAbsolutePath());
-            ImageIO.write(ImageManipulation.MatrixToImage(newImage2.pixels, newImage2.width,newImage2.height,5), "jpg", new File(String.format("output/888new-test(%d).jpg", i++)));
+            ImageIO.write(ImageManipulation.MatrixToImage(newImage2.pixels, newImage2.width,newImage2.height,5), "jpg", new File(String.format("search/new-test(%d).jpg", i++)));
         }
 
     }
